3d4123b761b251b0a6e3d2b49b0428f789232caf
Lusine <lus.an@mail.ru>
2019-11-25 19:06:50 +0400

    Simple changes

--- app/src/components/container/App.jsx
+++ app/src/components/container/App.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react';
 import { connect } from 'react-redux';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../../assets/stylesheets/App.scss';
 
-import { setAllImages } from '../../redux/actions';
-import Routes from '../../routes/Routes';
 import AppNavbar from '../reactComponents/AppNavbar';
+import { Container } from 'react-bootstrap';
 import Details from '../presentational/Details';
 import HomePage from '../presentational/HomePage';
-import { Container } from 'react-bootstrap';
-import { getAllImages } from '../../data';
 import M from '../../messages/messages';
+import Routes from '../../routes/Routes';
+import { getAllImages } from '../../data';
+import { setAllImages } from '../../redux/actions';
 
 class App extends React.Component {
 
--- app/src/components/reactComponents/AppNavbar.jsx
+++ app/src/components/reactComponents/AppNavbar.jsx
@@ -14,10 +14,10 @@ class AppNavbar extends React.Component {
                     <Container>
                         <Nav className='mr-auto'>
                             <Nav.Link as={Link} to='/' >{M.nav.home}</Nav.Link>
-                            <Nav.Link as={Link} to='/gallery/monasteries' activeClassName='active'>{M.nav.monasteries}</Nav.Link>
-                            <Nav.Link as={Link} to='/gallery/history' activeClassName='active' >{M.nav.history}</Nav.Link>
-                            <Nav.Link as={Link} to='/gallery/nature' activeClassName='active' >{M.nav.nature}</Nav.Link>
-                            <Nav.Link as={Link} to='/gallery/favorites' activeClassName='active'>{M.nav.favorite}</Nav.Link>
+                            <Nav.Link as={Link} to='/gallery/monasteries'>{M.nav.monasteries}</Nav.Link>
+                            <Nav.Link as={Link} to='/gallery/history'>{M.nav.history}</Nav.Link>
+                            <Nav.Link as={Link} to='/gallery/nature'>{M.nav.nature}</Nav.Link>
+                            <Nav.Link as={Link} to='/gallery/favorites'>{M.nav.favorite}</Nav.Link>
                         </Nav>
                     </Container>
                 </Navbar.Collapse>
